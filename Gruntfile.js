74042f89394829a61eca739e76192d8bbe47ca32
Tim Paul <contact@timpaul.co.uk>
2015-06-20 16:29:54 +0100

    Remove references to Hogan

--- Gruntfile.js
+++ Gruntfile.js
@@ -115,21 +115,9 @@ module.exports = function(grunt){
     grunt.loadNpmTasks(task);
   });
 
-  grunt.registerTask(
-    'convert_template',
-    'Converts the govuk_template to use mustache inheritance',
-    function () {
-      var script = require(__dirname + '/lib/template-conversion.js');
-
-      script.convert();
-      grunt.log.writeln('govuk_template converted');
-    }
-  );
-
   grunt.registerTask('generate-assets', [
     'clean',
     'copy',
-    'convert_template',
     'replace',
     'sass'
   ]);
