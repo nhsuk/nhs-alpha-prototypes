20c50ddc909d39a20822a2b98467b7ed80c41081
Paul M Furley <paul@paulfurley.com>
2015-09-11 14:23:10 +0100

    Don't require username and password on heroku

--- server.js
+++ server.js
@@ -10,16 +10,6 @@ var path = require('path'),
     password = process.env.PASSWORD,
     env = process.env.NODE_ENV || 'development';
 
-// Authenticate against the environment-provided credentials, if running
-// the app in production (Heroku, effectively)
-if (env === 'production') {
-  if (!username || !password) {
-    console.log('Username or password is not set, exiting.');
-    process.exit(1);
-  }
-  app.use(express.basicAuth(username, password));
-}
-
 // Application settings
 app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
