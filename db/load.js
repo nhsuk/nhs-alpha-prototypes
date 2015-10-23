c16346b86fae25fb3b7da2db25ea5f8bb60893b5
Dominic Baggott <dominic.baggott@gmail.com>
2015-10-23 11:57:35 +0100

    Refactor load_data_files to return an object
    Return an object of key/value pairs for all the data. Add that data into
    `app.locals` outside of the `load_data_files` function.
    
    This should make it easier to manipulate the data in that structure
    before adding it to `app.locals`.

--- db/load.js
+++ db/load.js
@@ -1,15 +1,21 @@
 var fs = require('fs'),
-    Promise = require('bluebird');
+    Promise = require('bluebird'),
+    util = require('util');
 
 Promise.promisifyAll(fs);
 
 module.exports = {
   load: function(app) {
-    load_data_files(app);
+    load_data_files()
+      .done(function(data) {
+        Object.keys(data).forEach(function(key) {
+          app.locals[key] = data[key];
+        });
+      });
   }
 }
 
-function load_data_files(app) {
+function load_data_files() {
   return fs.readdirAsync(__dirname)
     .filter(function(file_name) {
       return file_name.match(/\.json$/);
@@ -18,8 +24,12 @@ function load_data_files(app) {
       return fs.readFileAsync(__dirname + '/' + file_name, 'utf8')
         .then(JSON.parse)
         .then(function(data) {
-          var dataset_name = file_name.replace(/\.json$/, '');
-          app.locals[dataset_name] = data;
+          var dataset_name = file_name.replace(/\.json$/, ''),
+              o = {};
+
+          o[dataset_name] = data;
+
+          return o;
         })
         .catch(SyntaxError, function(e) {
           console.error('invalid JSON in file ' + file_name);
@@ -29,5 +39,8 @@ function load_data_files(app) {
           console.log('unable to read file ' + file_name);
           throw(e);
         });
+    })
+    .reduce(function(a, b) {
+      return util._extend(a, b);
     });
 }
