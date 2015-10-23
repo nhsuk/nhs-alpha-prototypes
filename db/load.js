043bf12109c8036446a176e1d20485c31640dbbf
Dominic Baggott <dominic.baggott@gmail.com>
2015-10-23 07:09:44 +0100

    Extract load_data_files function in db/load.js
    We want to do some massaging of the data after we've loaded it. This is
    a stepping stone towards doing that in a less horrid way.

--- db/load.js
+++ db/load.js
@@ -2,23 +2,27 @@ var fs = require('fs');
 
 module.exports = {
   load: function(app) {
-    fs.readdir(__dirname, function(err, file_names) {
-      json_files = file_names.filter(function(name) {
-        return name.match(/\.json$/);
-      });
+    load_data_files(app);
+  }
+}
 
-      json_files.forEach(function(file_name) {
-        fs.readFile(__dirname + '/' + file_name, 'utf8', function(err, data) {
-          if (err) {
-            console.log('Could not load data from ' + file_name);
-            throw(err);
-          }
-          else {
-            var dataset_name = file_name.replace(/\.json$/, '');
-            app.locals[dataset_name] = JSON.parse(data);
-          }
-        });
+function load_data_files(app) {
+  fs.readdir(__dirname, function(err, file_names) {
+    json_files = file_names.filter(function(name) {
+      return name.match(/\.json$/);
+    });
+
+    json_files.forEach(function(file_name) {
+      fs.readFile(__dirname + '/' + file_name, 'utf8', function(err, data) {
+        if (err) {
+          console.log('Could not load data from ' + file_name);
+          throw(err);
+        }
+        else {
+          var dataset_name = file_name.replace(/\.json$/, '');
+          app.locals[dataset_name] = JSON.parse(data);
+        }
       });
     });
-  }
+  });
 }
